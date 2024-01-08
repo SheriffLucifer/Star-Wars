a4c65b0501c52eaf63f5be415a58eb0253965f54
Oleg <olegashepelev@gmail.com>
2024-01-08 22:41:02 +0600

    Complete Release 1. CharacterByQuery

--- practice/core-promises-star-wars/scripts/search.js
+++ practice/core-promises-star-wars/scripts/search.js
@@ -1,9 +1,53 @@
 // Методы, которые могут пригодиться:
-// starWars.searchCharacters(query), 
-// starWars.searchPlanets(query), 
+// starWars.searchCharacters(query),
+// starWars.searchPlanets(query),
 // starWars.searchSpecies(query).
-// starWars.getCharactersById(id), 
-// starWars.getPlanetsById(id), 
+// starWars.getCharactersById(id),
+// starWars.getPlanetsById(id),
 // starWars.getSpeciesById(id)
 
 // Тут ваш код.
+
+const input = document.querySelector(".input").value.trim();
+const searchButton = document.getElementById("byQueryBtn");
+const result = document.getElementById("result-container");
+const spinner = document.querySelector(".spinner");
+const msgHeader = document.querySelector(".message-header");
+const msgBody = document.querySelector(".message-body");
+
+searchButton.addEventListener("click", searchCharacters);
+
+function searchCharacters() {
+  const input = document.querySelector(".input").value.trim();
+  // Отобразить спиннер при выполнении запроса
+  spinner.style.visibility = "visible";
+
+  // Скрыть предыдущие результаты
+  msgHeader.textContent = "";
+  msgBody.textContent = "";
+
+  // Вызвать метод поиска персонажей
+  starWars
+    .searchCharacters(input)
+    .then((characters) => {
+      // Скрыть спиннер после получения результатов
+      spinner.style.visibility = "hidden";
+
+      // Показать результаты в блоке
+      result.style.visibility = "visible";
+      const character = characters.results[0];
+      msgHeader.textContent = character.name;
+
+      for (let key in character) {
+        const value = document.createElement("p");
+        value.textContent = `${key}: ${character[key]}`;
+        msgBody.appendChild(value);
+      }
+    })
+    .catch((error) => {
+      // Обработать ошибку, если она возникнет
+      console.error("Error searching characters:", error);
+      // Скрыть спиннер в случае ошибки
+      spinner.style.visibility = "hidden";
+    });
+}
