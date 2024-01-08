d4760fdd5adb6b07d39e880065cfe5a1204c90aa
Oleg <olegashepelev@gmail.com>
2024-01-09 00:25:55 +0600

    Complete Release 3. Select resource

--- practice/core-promises-star-wars/scripts/search.js
+++ practice/core-promises-star-wars/scripts/search.js
@@ -8,52 +8,79 @@
 
 // Тут ваш код.
 
-const input = document.querySelector(".input").value.trim();
 const searchButton = document.getElementById("byQueryBtn");
-const result = document.getElementById("result-container");
+const resultContainer = document.getElementById("result-container");
 const spinner = document.querySelector(".spinner");
 const msgHeader = document.querySelector(".message-header");
 const msgBody = document.querySelector(".message-body");
+const resourceSelect = document.getElementById("selection");
 
 searchButton.addEventListener("click", searchCharacters);
 
-function searchCharacters() {
+async function searchCharacters() {
   const input = document.querySelector(".input").value.trim();
+  const selectedResource = resourceSelect.value;
   // Отобразить спиннер при выполнении запроса
   spinner.style.visibility = "visible";
 
   // Скрыть предыдущие результаты
   msgHeader.textContent = "";
   msgBody.textContent = "";
+  resultContainer.style.visibility = "hidden";
 
-  // Вызвать метод поиска персонажей
-  starWars
-    .searchCharacters(input)
-    .then(async (characters) => {
-      // Скрыть спиннер после получения результатов
-      spinner.style.visibility = "hidden";
+  let searchMethod;
+  switch (selectedResource) {
+    case "people":
+      searchMethod = starWars.searchCharacters;
+      break;
+    case "planets":
+      searchMethod = starWars.searchPlanets;
+      break;
+    case "species":
+      searchMethod = starWars.searchSpecies;
+      break;
+    default:
+      console.error("Invalid resource selected");
+      return;
+  }
 
-      // Показать результаты в блоке
-      result.style.visibility = "visible";
-      const character = characters.results[0];
-      const homeworldId = character.homeworld.split("/").slice(-2, -1)[0];
+  try {
+    const results = await searchMethod(input);
+
+    // Скрыть спиннер после получения результатов
+    spinner.style.visibility = "hidden";
+
+    // Показать результаты в блоке
+    resultContainer.style.visibility = "visible";
+    const data = results.results[0];
+
+    // Если выбран ресурс "people", заменить ссылку на планету на её наименование
+    if (selectedResource === "people" && data && data.homeworld) {
+      const homeworldId = data.homeworld.split("/").slice(-2, -1)[0];
       const homeworldData = await starWars.getPlanetsById(homeworldId);
       const homeworldName = homeworldData.name;
 
       // Заменить ссылку на планету на её наименование в изначальных данных о персонаже
-      character.homeworld = homeworldName;
-      msgHeader.textContent = character.name;
+      data.homeworld = homeworldName;
+    }
 
-      for (let key in character) {
-        const value = document.createElement("p");
-        value.textContent = `${key}: ${character[key]}`;
-        msgBody.appendChild(value);
+    if (data && data.name) {
+      msgHeader.textContent = data.name;
+    }
+
+    for (let key in data) {
+      const value = document.createElement("p");
+      if (key === "homeworld" && selectedResource === "species") {
+        value.textContent = `${key}: ${data.homeworld}`;
+      } else {
+        value.textContent = `${key}: ${data[key]}`;
       }
-    })
-    .catch((error) => {
-      // Обработать ошибку, если она возникнет
-      console.error("Error searching characters:", error);
-      // Скрыть спиннер в случае ошибки
-      spinner.style.visibility = "hidden";
-    });
+      msgBody.appendChild(value);
+    }
+  } catch (error) {
+    // Обработать ошибку, если она возникнет
+    console.error("Error searching characters:", error);
+    // Скрыть спиннер в случае ошибки
+    spinner.style.visibility = "hidden";
+  }
 }
